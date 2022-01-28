dbf3a6978483e643d99d856be2531a4f7172776c
leandro konrath <leandro0.konrath@gmail.com>
2022-01-28 10:31:11 -0300

    routes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import usersRoute from './routes/users.route';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(usersRoute);
 
 
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -6,11 +6,26 @@ const usersRoute = Router();
 usersRoute.get('/users', (req:Request, res:Response) => {
     const users = [{userName: 'John'}];
     res.status(StatusCodes.OK).send(users);
-})
+});
 
-usersRoute.get('/users/:uuid', (req:Request, res:Response) => {
-    const uuid = req.params.uuid
+usersRoute.get('/users/:uuid', (req:Request<{uuid:string}>, res:Response) => {
+    const uuid = req.params.uuid;
     res.status(StatusCodes.OK).send({ uuid });
-})
+});
+
+usersRoute.post('/users', (req:Request, res:Response) => {
+    const newUser = req.body;
+    //console.log(req.body);
+    res.status(StatusCodes.CREATED).send(newUser);
+});
+
+usersRoute.put('/users/:uuid', (req:Request<{uuid:string}>, res:Response) => {
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({ uuid });
+});
+
+usersRoute.delete('/users/:uuid', (req:Request<{uuid:string}>, res:Response) => {
+    res.status(StatusCodes.OK);
+});
 
 export default usersRoute;
